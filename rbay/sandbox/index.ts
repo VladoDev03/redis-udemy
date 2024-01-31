719181c33fa4ed2e136860554cd8f87e7536a072
Vladimir Stoyanov <vladsto101@gmail.com>
2024-01-31 20:26:19 +0200

    Added hash data structure.

--- rbay/sandbox/index.ts
+++ rbay/sandbox/index.ts
@@ -1,5 +1,23 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+    // await client.del('car');
+
+    await client.hSet('car', {
+        color: 'red',
+        year: 1950
+    });
+
+    const car = await client.hGetAll('car');
+    // const car = await client.hGetAll('notExistingKey');
+
+    if (Object.keys(car).length === 0) {
+        console.log('Car not found, respond with 404');
+        return;
+    }
+
+    console.log(car);
+};
+
 run();
