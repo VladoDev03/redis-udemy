79a15df555d8996da7729283fa29fc0b77fc4798
Vladimir Stoyanov <vladsto101@gmail.com>
2024-02-01 02:25:10 +0200

    Added user signup (sessions) and items.

--- rbay/sandbox/index.ts
+++ rbay/sandbox/index.ts
@@ -4,20 +4,43 @@ import { client } from '../src/services/redis';
 const run = async () => {
     // await client.del('car');
 
-    await client.hSet('car', {
+    // await client.hSet('car', {
+    //     color: 'red',
+    //     year: 1950
+    // });
+
+    // const car = await client.hGetAll('car');
+    // // const car = await client.hGetAll('notExistingKey');
+
+    // if (Object.keys(car).length === 0) {
+    //     console.log('Car not found, respond with 404');
+    //     return;
+    // }
+
+    // console.log(car);
+
+    await client.hSet('car1', {
         color: 'red',
         year: 1950
     });
 
-    const car = await client.hGetAll('car');
-    // const car = await client.hGetAll('notExistingKey');
+    await client.hSet('car2', {
+        color: 'green',
+        year: 1955
+    });
 
-    if (Object.keys(car).length === 0) {
-        console.log('Car not found, respond with 404');
-        return;
-    }
+    await client.hSet('car3', {
+        color: 'blue',
+        year: 1960
+    });
 
-    console.log(car);
+    const commands = [1, 2, 3].map(id => {
+        return client.hGetAll(`car${id}`);
+    });
+
+    const results = await Promise.all(commands);
+
+    console.log(results);
 };
 
 run();
