961f23e47e9a00a2e9939ad1de97c2dde2a88e4f
Vladimir Stoyanov <vladsto101@gmail.com>
2024-02-02 19:47:18 +0200

    Added sign in.

--- rbay/src/services/keys.ts
+++ rbay/src/services/keys.ts
@@ -9,3 +9,5 @@ export const itemsKey = (itemId: string) => `items#${itemId}`;
 export const usernamesUniqueKey = () => 'usernames:unique';
 
 export const userLikesKey = (userId: string) => `users:likes#${userId}`;
+
+export const usernamesKey = () => 'usernames';
