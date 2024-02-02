a97fff68d6dcf617bcd5b9d0a8aabb28978dadfe
Vladimir Stoyanov <vladsto101@gmail.com>
2024-02-02 23:26:45 +0200

    Finished sort items by views.

--- rbay/src/services/queries/items/by-views.ts
+++ rbay/src/services/queries/items/by-views.ts
@@ -1,18 +1,37 @@
 import { client } from "$services/redis";
 import { itemsKey, itemsByViewsKey } from "$services/keys";
+import { deserialize } from "./deserialize";
 
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
-    const results = await client.sort(
+    let results: any = await client.sort(
         itemsByViewsKey(),
         {
             GET: [
                 '#',
                 `${itemsKey('*')}->name`,
                 `${itemsKey('*')}->views`,
+                `${itemsKey('*')}->endingAt`,
+                `${itemsKey('*')}->imageUrl`,
+                `${itemsKey('*')}->price`
             ],
-            BY: 'score'
+            BY: 'nosort',
+            DIRECTION: order,
+            LIMIT: {
+                offset,
+                count
+            }
         }
     );
 
-    console.log(results);
+    const items = [];
+
+    while (results.length) {
+        const [id, name, views, endingAt, imageUrl, price, ...rest] = results;
+        const item = deserialize(id, { name, views, endingAt, imageUrl, price });
+
+        items.push(item);
+        results = rest;
+    }
+
+    return items;
 };
